bb5eff2d66dfd299d351fd6fe2102fdab42f54c2
Ben Tristem <github@bentristem.com>
2017-03-03 13:29:24 +0000

    5_CC_RPG Enemy Floating Health Bar

---
+++ Assets/Enemies/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float maxHealthPoints = 100f;
+
+    float currentHealthPoints = 100f;
+
+    public float healthAsPercentage
+    {
+        get
+        {
+            return currentHealthPoints / maxHealthPoints;
+        }
+    }
+}
