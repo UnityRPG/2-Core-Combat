6431b034cd7540b798cab1e69b41bb6ad67f8463
Ben Tristem <github@bentristem.com>
2017-03-03 10:10:02 +0000

    4_CC_RPG Player Health Bar UI

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float maxHealthPoints = 100f;
+
+    float currentHealthPoints = 100f;
+
+    public float healthAsPercentage
+    {
+        get
+        {
+            return currentHealthPoints / maxHealthPoints;
+        }
+    }
+}
