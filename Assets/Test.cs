c1c4d8922c732c0e75d54f3d6363d51907ae5046
Ben Tristem <github@bentristem.com>
2017-03-07 15:45:15 +0000

    Extended Unity's Invoke and InvokeRepeating

---
+++ Assets/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    Coroutine myHandle = null;
+
+	// Use this for initialization
+	void Start () {
+        myHandle = this.InvokeRepeating(Donkey, 1f, 1f);
+        this.Invoke(CancelMe, 5f);
+	}
+
+    void CancelMe() {
+        StopCoroutine(myHandle);
+    }
+
+    void Donkey() {
+        print("lksadf");
+    }
+}
---
+++ Assets/Utility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public static class Utility
+{
+    // Replace Unity's built-in string-referenced Invoke
+    public static Coroutine Invoke(this MonoBehaviour monoBehaviour, Action action, float delay)
+    {
+        return monoBehaviour.StartCoroutine(InvokeImpl(action, delay));
+    }
+
+    private static IEnumerator InvokeImpl(Action action, float time)
+    {
+        yield return new WaitForSeconds(time);
+        action();
+    }
+
+
+    public static Coroutine InvokeRepeating(this MonoBehaviour monoBehaviour, Action action, float initialDelay, float repeatRate)
+    {
+        return monoBehaviour.StartCoroutine(InvokeRepeatingImpl(action, initialDelay, repeatRate));
+    }
+
+    private static IEnumerator InvokeRepeatingImpl(Action action, float initialDelay, float repeatRate)
+    {
+        yield return new WaitForSeconds(initialDelay);
+        while (true)
+        {
+            action();
+            yield return new WaitForSeconds(repeatRate);
+        }
+    }
+}
