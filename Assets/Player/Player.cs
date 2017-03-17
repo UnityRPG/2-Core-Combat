3d1fc298c2fd321040eabefcf0c3c2b162c73f07
Ben Tristem <github@bentristem.com>
2017-03-17 17:49:38 +0000

    25_CC_RPG Using Interfaces In C#

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour, IDamageable {
 
     [SerializeField] float maxHealthPoints = 100f;
 
     float currentHealthPoints = 100f;
 
-    public float healthAsPercentage
+    public float healthAsPercentage { get { return currentHealthPoints / maxHealthPoints; }}
+
+    public void TakeDamage(float damage)
     {
-        get
-        {
-            return currentHealthPoints / maxHealthPoints;
-        }
+        currentHealthPoints = Mathf.Clamp(currentHealthPoints - damage, 0f, maxHealthPoints);
     }
 }
---
+++ Assets/Player/Projectile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+        print("Projectile hit " + collider.gameObject);
+    }
+}
