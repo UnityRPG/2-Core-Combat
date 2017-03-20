36c82baf87a23f25f35750be4def500acef2eafc
Ben Tristem <github@bentristem.com>
2017-03-20 18:49:12 +0000

    27_CC_RPG Enemy Move And Attack Spheres

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour, IDamageable {
 
     [SerializeField] float maxHealthPoints = 100f;
     [SerializeField] float attackRadius = 4f;
+    [SerializeField] float chaseRadius = 6f;
 
     float currentHealthPoints = 100f;
     AICharacterControl aiCharacterControl = null;
@@ -30,11 +31,28 @@ public class Enemy : MonoBehaviour, IDamageable {
         float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
         if (distanceToPlayer <= attackRadius)
         {
+            print(gameObject.name + " attacking player");
+            // TODO spawn projectile
+        }
+
+        if (distanceToPlayer <= chaseRadius)
+        {
             aiCharacterControl.SetTarget(player.transform);
         }
         else
         {
             aiCharacterControl.SetTarget(transform);
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw attack sphere 
+        Gizmos.color = new Color(255f, 0, 0, .5f);
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+
+        // Draw chase sphere 
+        Gizmos.color = new Color(0, 0, 255, .5f);
+        Gizmos.DrawWireSphere(transform.position, chaseRadius);
     }
 }
