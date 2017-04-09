8f36ffd6d13a5665c4e852102c36b17b97725ffb
Ben Tristem <github@bentristem.com>
2017-04-09 18:34:17 +0100

    50_CC_RPG Problems With Prefabs

--- Assets/Characters/Player/Player.cs
+++ Assets/Characters/Player/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Player : MonoBehaviour, IDamageable {
 
@@ -12,7 +13,6 @@ public class Player : MonoBehaviour, IDamageable {
     [SerializeField] float maxAttackRange = 2f;
 
     [SerializeField] Weapon weaponInUse;
-    [SerializeField] GameObject weaponSocket;
 
     GameObject currentTarget;
     float currentHealthPoints;
@@ -30,10 +30,20 @@ public class Player : MonoBehaviour, IDamageable {
 
     private void PutWeaponInHand()
     {
-        var weaponPrefab = weaponInUse.GetWeaponPrefab();   
-        var weapon = Instantiate(weaponPrefab, weaponSocket.transform);
+        var weaponPrefab = weaponInUse.GetWeaponPrefab();
+        GameObject dominantHand = RequestDominantHand();
+        var weapon = Instantiate(weaponPrefab, dominantHand.transform);
         weapon.transform.localPosition = weaponInUse.gripTransform.localPosition;
         weapon.transform.localRotation = weaponInUse.gripTransform.localRotation;
+    }
+
+    private GameObject RequestDominantHand()
+    {
+        var dominantHands = GetComponentsInChildren<DominantHand>();
+        int numberOfDominantHands = dominantHands.Length;
+        Assert.IsFalse(numberOfDominantHands <= 0, "No DominantHand found on Player, please add one");
+        Assert.IsFalse(numberOfDominantHands >  1, "Multiple DominantHand scripts on Player, please remove one");
+        return dominantHands[0].gameObject;
     }
 
     private void RegisterForMouseClick()
@@ -42,6 +52,7 @@ public class Player : MonoBehaviour, IDamageable {
         cameraRaycaster.notifyMouseClickObservers += OnMouseClick;
     }
 
+    // TODO refactor to reduce number of lines
     void OnMouseClick(RaycastHit raycastHit, int layerHit)
     {
         if (layerHit == enemyLayer)
